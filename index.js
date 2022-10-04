2c82852e56e6fc9f44a3270940dfc0f47a4f7053
Oliver Catelan-Robinson <oscr1998@outlook.com>
2022-10-04 15:20:30 +0100

    working, submit posts, submit comments, increase reaction Num

---
+++ index.js
@@ -0,0 +1,187 @@
+const form = document.getElementById('submit_form')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    // const postText = document.getElementById('postText').value
+    console.log("postText test", e.target.postText)
+    
+    const object = {
+        entry: e.target.postText.value,
+        comments: [],
+        reactions: {
+            smiley: 0,
+            sad: 0,
+            like: 0,
+        },
+        gif: ""
+    };
+
+    fetch("http://localhost:3000/entries/", {
+        method: "POST",
+
+        body: JSON.stringify(object),
+
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    })
+})
+
+const section = document.getElementById('entries')
+console.log(section)
+
+async function fetchData(){
+    const rawData = await fetch("http://localhost:3000/entries/")
+    const commentData = await rawData.json()
+    console.log(commentData, "from fetch data function")
+
+    commentData.forEach((i) => {
+        let div = document.createElement('div')
+        div.className = 'div_post'
+        div.setAttribute("id", i.id)
+        section.appendChild(div)
+        
+        let p = document.createElement('p')
+        p.className = "p_entry"
+        p.textContent = i.entry
+        div.appendChild(p);
+
+        let reaction_smile = document.createElement('button')
+        // reaction_smile.id = `smile${i.id}`
+        reaction_smile.setAttribute("id", `smile${i.id}`)
+        reaction_smile.textContent = `Smile: ${i.reactions.smiley}`
+        reaction_smile.addEventListener('click', async () => {
+            console.log(`http://localhost:3000/entries/${i.id}/reaction`)
+            await fetch(`http://localhost:3000/entries/${i.id}/reaction`, {
+            method: "PATCH",
+
+            body: JSON.stringify({reaction: "smiley"}),
+                
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+                }
+            })
+            window.location.href=window.location.href
+            // Page.MaintainScrollPositionOnPostBack = True
+        })
+        div.appendChild(reaction_smile)
+
+        let reaction_sad = document.createElement('button')
+        reaction_sad.id = `sad${i.id}`
+        reaction_sad.textContent = `Sad: ${i.reactions.sad}`
+        reaction_sad.addEventListener('click', async () => {
+            console.log(`http://localhost:3000/entries/${i.id}/reaction`)
+            await fetch(`http://localhost:3000/entries/${i.id}/reaction`, {
+            method: "PATCH",
+
+            body: JSON.stringify({reaction: "sad"}),
+                
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+                }
+            })
+            window.location.href=window.location.href
+        })
+        div.appendChild(reaction_sad)
+
+        let reaction_like = document.createElement('button')
+        reaction_like.id = `like${i.id}`
+        reaction_like.textContent = `like: ${i.reactions.like}`
+        reaction_like.addEventListener('click', async () => {
+            console.log(`http://localhost:3000/entries/${i.id}/reaction`)
+            await fetch(`http://localhost:3000/entries/${i.id}/reaction`, {
+            method: "PATCH",
+
+            body: JSON.stringify({reaction: "like"}),
+                
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+                }
+            })
+            window.location.href=window.location.href
+        })
+        div.appendChild(reaction_like)
+
+        let reply = document.createElement('button')
+        reply.textContent= `Reply (Number of replies ${i.comments.length})`
+
+        reply.addEventListener('click', async () => {
+            let commentForm = document.createElement('form')
+            commentForm.className = "commentForm"
+            commentForm.addEventListener('submit', async (e) => {
+                console.log("testing eTarget", e.target.postReply.value)
+                await fetch(`http://localhost:3000/entries/${i.id}/comments`, {
+                    method: "PATCH",
+
+                    body: JSON.stringify({comment: e.target.postReply.value}),
+                
+                    headers: {
+                        "Content-type": "application/json; charset=UTF-8"
+                        }
+            
+                })
+            })
+            div.append(commentForm)
+
+            let commentForm_input = document.createElement('input')
+            commentForm_input.type = "text"
+            commentForm_input.id = "postReply"
+            commentForm.append(commentForm_input)
+
+            let commentForm_submit = document.createElement('input')
+            commentForm_submit.type = "submit"
+            commentForm_submit.value = "Submit reply"
+            commentForm.append(commentForm_submit)
+        })
+
+        div.append(reply)
+
+
+    });
+}
+
+fetchData()
+
+// .then(() => {
+//     const smileButton = document.querySelectorAll('.div_post')
+//     console.log("testing smile button", smileButton)
+
+//     smileButton.addEventListener('click', () => {
+//     console.log('testing smile')}
+// )
+// })
+
+
+
+// smileButton.addEventListener('click', () => {
+
+//     console.log('testing smile')
+
+//     fetch(`http://localhost:3000/entries/1`, {
+//         method: "PATCH",
+
+//         body: JSON.stringify(
+//             {reactions: smiley}
+//         ),
+
+//         headers: {
+//             "Content-type": "application/json; charset=UTF-8"
+//         }
+//     })
+// })
+
+
+// const functionIncrement = async () => {
+//     console.log('testing increment')
+//     fetch(`http://localhost:3000/entries`, {
+//         method: "PATCH",
+
+//         body: JSON.stringify(
+//             {reactions: smiley}
+//         ),
+
+//         headers: {
+//             "Content-type": "application/json; charset=UTF-8"
+//         }
+//     })
+// }
