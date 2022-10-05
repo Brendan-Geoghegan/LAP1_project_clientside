30ba611f878c5ccd33bad174ca1452df0217486c
Oliver Catelan-Robinson <oscr1998@outlook.com>
2022-10-05 10:21:57 +0100

    added random gif functionality

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const form = document.getElementById('submit_form')
-
 let object = {
     entry: "",
     comments: [],
@@ -11,20 +9,39 @@ let object = {
     gif: ""
 };
 
-const addGif = document.getElementById("addGif")
-addGif.addEventListener('click', async (e) => {
+const gifForm = document.getElementById('gif_form')
+gifForm.addEventListener('submit', async (e) =>{
     e.preventDefault()
-    const gifData = await fetch("https://api.giphy.com/v1/gifs/search?api_key=0S3uHG2JHXGiL6ooiPgmsyrS25ufBlGf&q=dog&limit=6&offset=0&rating=g&lang=en")
+    console.log('gif input test')
+    console.log(e.target.gifInput.value)
+    const gifResult = e.target.gifInput.value
+    const gifData = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=0S3uHG2JHXGiL6ooiPgmsyrS25ufBlGf&q=${gifResult}&limit=20&offset=0&rating=g&lang=en`)
     const gifJson = await gifData.json();
     console.log(gifJson)
+    
 
-    // gifJson.data.forEach(img => {
-    //     console.log('lots of images', img.images.original.url)
-    // })
-
-    object.gif = gifJson.data[0].images.original.url
-
+    object.gif = gifJson.data[(Math.floor(Math.random()*20))].images.original.url
 })
+
+// const addGif = document.getElementById("addGif")
+// addGif.addEventListener('click', async (e) => {
+//     e.preventDefault()
+//     const gifSearch = document.getElementById('gifInput')
+//     const gifResult = gifSearch.textContent
+//     console.log(gifResult)
+//     const gifData = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=0S3uHG2JHXGiL6ooiPgmsyrS25ufBlGf&q=${gifResult}&limit=6&offset=0&rating=g&lang=en`)
+//     const gifJson = await gifData.json();
+//     console.log(gifJson)
+    
+//     // gifJson.data.forEach(img => {
+//     //     console.log('lots of images', img.images.original.url)
+//     // })
+
+//     object.gif = gifJson.data[0].images.original.url
+
+// })
+
+const form = document.getElementById('submit_form')
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
